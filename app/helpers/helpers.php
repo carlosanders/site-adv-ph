051b972cbe2ccb3be6a823d1a8b6b661eb84b090
Carlos Anders <carlosanders@gmail.com>
2024-02-06 20:43:45 -0300

    organização da arquitetura

--- app/helpers/helpers.php
+++ app/helpers/helpers.php
@@ -1,14 +1,20 @@
 <?php
 
+use League\Plates\Extension\Asset;
+
 function view(string $view, array $data = [])
 {
     $path = dirname(__FILE__, 2) . DIRECTORY_SEPARATOR . 'views';
-
-    // var_dump($path);
+    //$assets = getcwd() . '/assets/';
+    //$assets = getcwd() . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR;
+    // $assets = dirname(__FILE__, 3) . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR;
+    // var_dump();    // die();
+    // var_dump(getcwd());
     // die();
 
     // Create new Plates instance
     $templates = new League\Plates\Engine($path);
+    $templates->loadExtension(new Asset(getcwd(), false));
 
     // Render a template
     echo $templates->render($view, $data);
--- app/views/master.php
+++ app/views/master.php
@@ -5,12 +5,19 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site com Slim Framework</title>
+    <link rel="stylesheet" href="<?= $this->asset('assets/bootstrap/css/bootstrap.min.css') ?>" />
 </head>
 
 <body>
     <div id="container">
         <?php echo $this->section('content') ?>
     </div>
+</body>
+
+<!-- JavaScript asset -->
+<script src="<?= $this->asset('assets/js/jquery-3.7.1.js'); ?>"></script>
+<script src="<?= $this->asset('assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
+<script src="<?= $this->asset('assets/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
 </body>
 
 </html>
